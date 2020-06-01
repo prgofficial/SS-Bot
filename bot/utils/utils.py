c8a8275976e2b5db1e08b4175a4c25adebf1eca6
PrG <65294906+prgofficial@users.noreply.github.com>
2020-06-01 15:39:40 +0530

    Update utils.py

--- bot/utils/utils.py
+++ bot/utils/utils.py
@@ -403,7 +403,7 @@ async def sample_fn(c, m):
                 supports_streaming=True
             )
         
-        await edit_message_text(m, text=f'Successfully completed in {datetime.timedelta(seconds=int(time.time()-start_time))}\n\nJoin @moviesonlydiscussion')
+        await edit_message_text(m, text=f'Successfully completed in {datetime.timedelta(seconds=int(time.time()-start_time))}\n\nJoin @moviesonlydiscussion\n\n©️ @prgofficial')
         c.CURRENT_PROCESSES[chat_id] -= 1
         
     except:
@@ -524,7 +524,7 @@ async def trim_fn(c, m):
             supports_streaming=True
         )
         
-        await snt.edit_text(f'Successfully completed process in {datetime.timedelta(seconds=int(time.time()-start_time))}\n\nJoin @moviesonlydiscussion')
+        await snt.edit_text(f'Successfully completed process in {datetime.timedelta(seconds=int(time.time()-start_time))}\n\nJoin @moviesonlydiscussion\n\n©️ @prgofficial')
         c.CURRENT_PROCESSES[chat_id] -= 1
         
     except:
