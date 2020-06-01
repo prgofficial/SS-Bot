30efd21fff892bba3c93af3226df1f0046fa874e
PrG <65294906+prgofficial@users.noreply.github.com>
2020-06-01 14:09:23 +0530

    Update start.py

--- bot/plugins/start.py
+++ bot/plugins/start.py
@@ -15,16 +15,16 @@ async def start(c, m):
         )
     
     await m.reply_text(
-        text=f"Hi there {m.from_user.first_name}.\n\nI'm Screenshot Generator Bot. I can provide screenshots from your video files with out downloading the entire file (almost instantly). For more details check /help.",
+        text=f"Namaskaaram😜 {m.from_user.first_name}.\n\nI'm a simple Screenshot Generator Bot. I can provide screenshots from your video files with out downloading the entire file (almost instantly). For more details check /help.",
         quote=True,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton('Source 😜', url='https://github.com/sahadz/screenShot-Bot'),
-                    InlineKeyboardButton('Project Channel', url='https://t.me/Super_Botz')
+                    InlineKeyboardButton('Source Code', url='https://github.com/prgofficial/SS-Bot'),
+                    InlineKeyboardButton('Support Group', url='https://t.me/moviesonlybotupdates')
                 ],
                 [
-                    InlineKeyboardButton('My Boss', url='https://t.me/Ankith_m')
+                    InlineKeyboardButton('Any Further Queries', url='https://t.me/prgofficial)
                 ]
             ]
         )
